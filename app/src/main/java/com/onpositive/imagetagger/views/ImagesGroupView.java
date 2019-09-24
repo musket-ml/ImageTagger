8b0cd9f25104f248990de3260dccb0b73b48d798
VasiliyLysokobylko <vasily@onpositive.com>
2019-09-24 19:52:41 +0700

    rename interface MainView to ImagesGroupView

--- app/src/main/java/com/onpositive/imagetagger/views/ImagesGroupView.java
+++ app/src/main/java/com/onpositive/imagetagger/views/ImagesGroupView.java
@@ -2,7 +2,7 @@ package com.onpositive.imagetagger.views;
 
 import java.util.List;
 
-public interface MainView {
+public interface ImagesGroupView {
     void showTaggedImages(List<String> taggedImages);
     void makeTaggedImage();
 }
