d96547ae2a7c2d07cd7f9e6e301e73edb779d5a9
VasiliyLysokobylko <vasily@onpositive.com>
2019-09-29 23:28:08 +0700

    (Tag) implements Comparable

--- app/src/main/java/com/onpositive/imagetagger/models/Tag.java
+++ app/src/main/java/com/onpositive/imagetagger/models/Tag.java
@@ -1,11 +1,40 @@
 package com.onpositive.imagetagger.models;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "tags")
-public class Tag {
+public class Tag implements Comparable<Tag>{
 
-    @PrimaryKey(autoGenerate = true) private int tagId;
+    @PrimaryKey(autoGenerate = true)
+    private int tagId;
     private String tagLabel;
+
+    public int getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTagLabel() {
+        return tagLabel;
+    }
+
+    public void setTagLabel(String tagLabel) {
+        this.tagLabel = tagLabel;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return tagLabel;
+    }
+
+    @Override
+    public int compareTo(Tag tag) {
+        return this.getTagLabel().compareTo(tag.getTagLabel());
+    }
 }
