196b597ae44d719a6bc56a467c6d514e23658d3c
VasiliyLysokobylko <vasily@onpositive.com>
2019-09-30 22:54:10 +0700

    (AppDatabase) add Tag entity

--- app/src/main/java/com/onpositive/imagetagger/data/AppDatabase.java
+++ app/src/main/java/com/onpositive/imagetagger/data/AppDatabase.java
@@ -1,9 +1,12 @@
 package com.onpositive.imagetagger.data;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.onpositive.imagetagger.models.Tag;
 import com.onpositive.imagetagger.models.TagDao;
 
+@Database(entities = {Tag.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract TagDao tagDao();
 }
