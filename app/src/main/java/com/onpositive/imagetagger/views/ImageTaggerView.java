b4be3a248528436a2fb3af328f33a7e4b82831f7
VasiliyLysokobylko <vasily@onpositive.com>
2019-10-31 02:28:57 +0700

    add interface ImageTaggerView

---
+++ app/src/main/java/com/onpositive/imagetagger/views/ImageTaggerView.java
@@ -0,0 +1,11 @@
+package com.onpositive.imagetagger.views;
+
+import com.onpositive.imagetagger.models.Tag;
+
+import java.util.List;
+
+public interface ImageTaggerView {
+    void showTags(List<Tag> tags);
+
+    void showNewTagDialog();
+}
