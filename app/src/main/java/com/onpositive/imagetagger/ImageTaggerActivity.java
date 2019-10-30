6589cc1440cbdfedafa8f2fba5230a6ffb590b04
VasiliyLysokobylko <vasily@onpositive.com>
2019-10-31 02:32:02 +0700

    (ImageTaggerActivity) add tags and tag creation dialog

--- app/src/main/java/com/onpositive/imagetagger/ImageTaggerActivity.java
+++ app/src/main/java/com/onpositive/imagetagger/ImageTaggerActivity.java
@@ -1,37 +1,58 @@
 package com.onpositive.imagetagger;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.onpositive.imagetagger.models.Tag;
+import com.onpositive.imagetagger.presenters.ImageTaggerPresenter;
 import com.onpositive.imagetagger.tools.Logger;
+import com.onpositive.imagetagger.views.ImageTaggerView;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class ImageTaggerActivity extends AppCompatActivity {
+public class ImageTaggerActivity extends AppCompatActivity implements ImageTaggerView {
 
     private static Logger log = new Logger(ImageTaggerActivity.class);
+    @BindView(R.id.makeTagFAB)
+    public FloatingActionButton makeTagFAB;
     @BindView(R.id.photo_iv)
     ImageView photoIV;
     @BindView(R.id.close_btn)
     Button closeBtn;
     @BindView(R.id.tags_rv)
     RecyclerView tagsRV;
+    private ImageTagsAdapter tagsRVAdapter;
 
-    @OnClick({R.id.close_btn})
-    public void onCick(View view) {
+    private ImageTaggerPresenter presenter;
+
+    @OnClick({R.id.close_btn, R.id.makeTagFAB})
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.close_btn:
                 log.log("Close button clicked");
                 onBackPressed();
+                break;
+            case R.id.makeTagFAB:
+                log.log("Make tag button clicked");
+                presenter.onMakeTagClicked();
                 break;
         }
     }
@@ -41,10 +62,77 @@ public class ImageTaggerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_tagger);
         ButterKnife.bind(this);
+        if (savedInstanceState == null) {
+            presenter = new ImageTaggerPresenter();
+        } else {
+            presenter = PresenterManager.getInstance().restorePresenter(savedInstanceState);
+        }
         Intent intent = getIntent();
-        String currentPhotoPath = intent.getStringExtra(TaggedImagesFragment.ARG_CURRENT_PHOTO_PATH);
+        String currentPhotoPath = intent.getStringExtra(ImagesGroupFragment.ARG_CURRENT_PHOTO_PATH);
         photoIV.setImageBitmap(BitmapFactory.decodeFile(currentPhotoPath));
-        System.out.println();
+        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
+        tagsRV.setLayoutManager(new LinearLayoutManager(this));
+        tagsRV.setItemAnimator(itemAnimator);
+        presenter.onCreate();
         log.log("onCreate executed.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        presenter.bindView(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        presenter.unbindView();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        PresenterManager.getInstance().savePresenter(presenter, outState);
+    }
+
+    @Override
+    public void showTags(List<Tag> tags) {
+        try {
+            tagsRVAdapter = new ImageTagsAdapter();
+            tagsRVAdapter.addAll(tags);
+            tagsRV.setAdapter(tagsRVAdapter);
+            log.log("Tags Recycler View loaded.");
+        } catch (Exception e) {
+            log.log("Failed tagsRV data loading: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void showNewTagDialog() {
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle(getResources().getString(R.string.new_tag_dialog_title))
+                .setView(input)
+                .setPositiveButton(
+                        getResources().getText(R.string.add),
+                        (dialog, which) -> {
+                            String tagLabel = input.getText().toString();
+                            presenter.onNewTagInputed(tagLabel);
+                            log.log("Add button clicked");
+                        })
+                .setNegativeButton(
+                        getResources().getText(R.string.cancel),
+                        (dialog, which) -> {
+                            log.log("Cancel button clicked");
+                        }
+                );
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        dialog.show();
     }
 }
