23d9cdfda298eb682ae60e9a3ce837225db84729
VasiliyLysokobylko <vasily@onpositive.com>
2019-11-01 18:23:58 +0700

    (TagPresenter) add onClick method

--- app/src/main/java/com/onpositive/imagetagger/presenters/TagPresenter.java
+++ app/src/main/java/com/onpositive/imagetagger/presenters/TagPresenter.java
@@ -4,6 +4,13 @@ import com.onpositive.imagetagger.models.Tag;
 import com.onpositive.imagetagger.views.TagView;
 
 public class TagPresenter extends BasePresenter<Tag, TagView> {
+
+    public void onClick() {
+        model.setChecked(
+                !model.isChecked());
+        view().setSelection(model.isChecked());
+    }
+
     @Override
     protected void updateView() {
         view().setTagLabel(model.getTagLabel());
