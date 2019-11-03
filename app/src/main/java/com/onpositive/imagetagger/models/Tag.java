5da70fc64456bfaac8005c825c9561bdcb50e55a
VasiliyLysokobylko <vasily@onpositive.com>
2019-11-03 23:50:36 +0700

    rename tags table

--- app/src/main/java/com/onpositive/imagetagger/models/Tag.java
+++ app/src/main/java/com/onpositive/imagetagger/models/Tag.java
@@ -5,7 +5,7 @@ import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "tags")
+@Entity
 public class Tag implements Comparable<Tag>{
 
     @PrimaryKey(autoGenerate = true)
