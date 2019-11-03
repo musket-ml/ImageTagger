82d275a662fac67c2d24abe277aa7a2b126f07db
VasiliyLysokobylko <vasily@onpositive.com>
2019-11-03 19:23:33 +0700

    add image DAO

--- app/src/main/java/com/onpositive/imagetagger/data/AppDatabase.java
+++ app/src/main/java/com/onpositive/imagetagger/data/AppDatabase.java
@@ -3,10 +3,14 @@ package com.onpositive.imagetagger.data;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.onpositive.imagetagger.models.Image;
+import com.onpositive.imagetagger.models.ImageTag;
 import com.onpositive.imagetagger.models.Tag;
 import com.onpositive.imagetagger.models.TagDao;
 
-@Database(entities = {Tag.class}, version = 1)
+@Database(entities = {Tag.class, Image.class, ImageTag.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract TagDao tagDao();
+    public abstract Image imageDao();
+    public abstract ImageTag imageTagDao();
 }
