eaf7ec2a3066a646861f560eeecc983c366d96c9
VasiliyLysokobylko <vasily@onpositive.com>
2019-11-03 23:53:55 +0700

    add tagged image DAO

---
+++ app/src/main/java/com/onpositive/imagetagger/models/ImageTagDao.java
@@ -0,0 +1,31 @@
+package com.onpositive.imagetagger.models;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ImageTagDao {
+    @Query("SELECT * FROM image INNER JOIN image_tag_join ON " +
+            "Image.imagePath=image_tag_join.imagePath WHERE " +
+            "image_tag_join.tagId = :tagId")
+    List<Image> getImagesForTag(final int tagId);
+
+    @Query("SELECT * FROM Tag INNER JOIN image_tag_join ON " +
+            "Tag.tagId=image_tag_join.tagId WHERE " +
+            "image_tag_join.imagePath = :imagePath")
+    List<Tag> getTagsForImage(final String imagePath);
+
+    @Insert
+    void insert(ImageTag imageTag);
+
+    @Update
+    void update(ImageTag imageTag);
+
+    @Delete
+    void delete(ImageTag imageTag);
+}
