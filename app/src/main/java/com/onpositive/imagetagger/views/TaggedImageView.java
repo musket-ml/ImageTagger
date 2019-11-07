dfaf14cf0fb175975a970b4522ad489632e5dd27
VasiliyLysokobylko <vasily@onpositive.com>
2019-11-07 19:31:28 +0700

    add view TaggedImageView

---
+++ app/src/main/java/com/onpositive/imagetagger/views/TaggedImageView.java
@@ -0,0 +1,7 @@
+package com.onpositive.imagetagger.views;
+
+import com.onpositive.imagetagger.models.TaggedImage;
+
+public interface TaggedImageView {
+    void showCard(TaggedImage taggedImage);
+}
