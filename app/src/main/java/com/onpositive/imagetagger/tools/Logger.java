696128b6df53baeeb4445ce8d352f34bc65b42fe
VasiliyLysokobylko <vasily@onpositive.com>
2019-11-14 17:39:24 +0700

    (Logger) append the names of the calling class and method to log

--- app/src/main/java/com/onpositive/imagetagger/tools/Logger.java
+++ app/src/main/java/com/onpositive/imagetagger/tools/Logger.java
@@ -9,22 +9,39 @@ public class Logger {
     private boolean isLogging = false;
 
     public Logger(Class c) {
-        logTag = c.getSimpleName();
+        logTag = c.getName();
         isLogging = BuildConfig.IS_LOGS_ENABLE;
+    }
+
+    public Logger() {
+        logTag = getCallerClass();
     }
 
     public void log(String message) {
         if (isLogging)
-            Log.d(logTag, message);
+            Log.d(logTag, getCallerName() + ": " + message);
     }
 
     public void error(String message) {
         if (isLogging)
-            Log.e(logTag, message);
+            Log.e(logTag, getCallerName() + ": " + message);
     }
 
     public void info(String message) {
         if (isLogging)
-            Log.i(logTag, message);
+            Log.i(logTag, getCallerName() + ": " + message);
+    }
+
+    private String getCallerName() {
+        StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+        StackTraceElement e = stacktrace[4];
+        String loggedMethodName = e.getMethodName();
+        return loggedMethodName;
+    }
+
+    private String getCallerClass() {
+        StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+        StackTraceElement e = stacktrace[4];
+        return e.getClassName();
     }
 }
