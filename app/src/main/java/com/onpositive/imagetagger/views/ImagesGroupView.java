f51de0b7088272c6a4bb617ad060415205480964
VasiliyLysokobylko <vasily@onpositive.com>
2019-11-19 15:03:33 +0700

    (ImagesGroupView) add file operations

--- app/src/main/java/com/onpositive/imagetagger/views/ImagesGroupView.java
+++ app/src/main/java/com/onpositive/imagetagger/views/ImagesGroupView.java
@@ -1,7 +1,10 @@
 package com.onpositive.imagetagger.views;
 
+import android.net.Uri;
+
 import com.onpositive.imagetagger.models.TaggedImage;
 
+import java.io.File;
 import java.util.List;
 
 public interface ImagesGroupView {
@@ -10,4 +13,10 @@ public interface ImagesGroupView {
     void makeTaggedImage();
 
     void startTaggedImageEditor(TaggedImage taggedImage);
+
+    File createTempFile(String fileName);
+
+    void exportFile(File file);
+
+    void writeFileContent(Uri uri, File contentFile);
 }
