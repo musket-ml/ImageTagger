0653efcd6c7d09fcfe82fedb4df7b54270447908
VasiliyLysokobylko <vasily@onpositive.com>
2019-12-13 17:37:28 +0700

    addition main view presenter

--- app/src/main/java/com/onpositive/imagetagger/MainActivity.java
+++ app/src/main/java/com/onpositive/imagetagger/MainActivity.java
@@ -8,17 +8,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.onpositive.imagetagger.presenters.MainPresenter;
 import com.onpositive.imagetagger.tools.Logger;
 import com.onpositive.imagetagger.views.Main;
 
 public class MainActivity extends AppCompatActivity implements Main {
 
     private Logger log = new Logger();
+    MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            presenter = new MainPresenter();
+        } else {
+            presenter = PresenterManager.getInstance().restorePresenter(savedInstanceState);
+        }
+        presenter.bindView(this);
+        presenter.onCreate();
+        log.log("onCreate executed. Tabs load complete.");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        presenter.bindView(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        presenter.unbindView();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        PresenterManager.getInstance().savePresenter(presenter, outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        log.log("onActivityResult");
+    }
+
+    @Override
+    public void showImagesFragment() {
         ImagesGroupFragment fragment = new ImagesGroupFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
--- app/src/main/java/com/onpositive/imagetagger/presenters/MainPresenter.java
+++ app/src/main/java/com/onpositive/imagetagger/presenters/MainPresenter.java
@@ -1,17 +1,17 @@
 package com.onpositive.imagetagger.presenters;
 
-import com.onpositive.imagetagger.MainActivity;
 import com.onpositive.imagetagger.models.TaggedImage;
+import com.onpositive.imagetagger.views.Main;
 
 import java.util.List;
 
-public class MainPresenter extends BasePresenter<List<TaggedImage>, MainActivity> {
+public class MainPresenter extends BasePresenter<List<TaggedImage>, Main> {
     @Override
     protected void updateView() {
 
     }
 
-    public void onMakeImageClicked(){
-
+    public void onCreate() {
+        view().showImagesFragment();
     }
 }
